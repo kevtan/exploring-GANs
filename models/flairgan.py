bbcddc1c73118b886f3d876a1f6ba5f5c887f7b7
Madeleine Yip <madeleinejyip@gmail.com>
2021-05-06 22:17:18 -0700

    First pass at DCGAN

--- models/flairgan.py
+++ models/flairgan.py
@@ -1,4 +1,60 @@
 import numpy as np
+from scipy.stats import truncnorm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+def show_tensor_images(image_tensor, num_images=16, size=(3, 64, 64), nrow=3):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images,
+    size per image, and images per row, plots and prints the images in an uniform grid.
+    '''
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu().clamp_(0, 1)
+    image_grid = make_grid(image_unflat[:num_images], nrow=nrow, padding=0)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.axis('off')
+    plt.show()
+
+def get_truncated_noise(n_samples, z_dim, truncation):
+    '''
+    Function for creating truncated noise vectors: Given the dimensions (n_samples, z_dim)
+    and truncation value, creates a tensor of that shape filled with random
+    numbers from the truncated normal distribution.
+    Parameters:
+        n_samples: the number of samples to generate, a scalar
+        z_dim: the dimension of the noise vector, a scalar
+        truncation: the truncation value, a non-negative scalar
+    '''
+    truncated_noise = truncnorm.rvs(-truncation, truncation, size=(n_samples, z_dim))
+    return torch.Tensor(truncated_noise)
+
+class MappingLayers(nn.Module):
+    '''
+    Mapping Layers Class
+    Values:
+        z_dim: the dimension of the noise vector, a scalar
+        hidden_dim: the inner dimension, a scalar
+        w_dim: the dimension of the intermediate noise vector, a scalar
+        lrmul: equalized learning rate to scale weights, a scalar
+    '''
+ 
+    def __init__(self, z_dim, hidden_dim, w_dim, lrmul=0.2):
+        super().__init__()
+        self.mapping = nn.Sequential(
+            nn.Linear(z_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, w_dim)
+        )
+        self.mul = lrmul
+
+    def forward(self, noise):
+        '''
+        Function for completing a forward pass of MappingLayers: 
+        Given an initial noise tensor, returns the intermediate noise tensor.
+        Parameters:
+            noise: a noise tensor with dimensions (n_samples, z_dim)
+        '''
+        return self.mapping(noise)
